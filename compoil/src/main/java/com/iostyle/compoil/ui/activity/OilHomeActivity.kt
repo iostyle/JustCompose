56ffafcf08759c421921b4acec5f94480a04c1e9
iostyle <developerlty@gmail.com>
2024-11-26 18:21:51 +0800

    feat: Scaffold top bar.

--- compoil/src/main/java/com/iostyle/compoil/ui/activity/OilHomeActivity.kt
+++ compoil/src/main/java/com/iostyle/compoil/ui/activity/OilHomeActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ListItem
 import androidx.compose.material.Text
@@ -19,6 +27,9 @@ import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,10 +40,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.iostyle.compoil.R
 import com.iostyle.compoil.bean.Records
 import com.iostyle.compoil.ui.dialog.CreateOilRecordsDialogCompose
 import com.iostyle.compoil.ui.theme.JustComposeTheme
@@ -49,7 +62,14 @@ class OilHomeActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContent {
             JustComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    // TODO 剥离组件并触发刷新
+//                    floatingActionButton = {
+//                        FloatAddRecordsButton(modifier = Modifier)
+//                    },
+                    topBar = { TopBar() }
+                ) { innerPadding ->
                     Box {
                         PullRefreshIndicatorView(modifier = Modifier.padding(innerPadding))
                     }
@@ -114,7 +134,8 @@ class OilHomeActivity : AppCompatActivity() {
                     show = true
                 }, shape = RoundedCornerShape(20.dp), modifier = modifier
                     .width(40.dp)
-                    .height(40.dp)
+                    .height(40.dp),
+                contentPadding = PaddingValues(0.dp)
             ) { Text(text = "+", fontSize = 20.sp, textAlign = TextAlign.Center) }
         }
 
@@ -146,4 +167,30 @@ class OilHomeActivity : AppCompatActivity() {
         }
     }
 
+    @Preview
+    @Composable
+    fun TopBar() {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .windowInsetsPadding(WindowInsets.statusBars)
+                .height(50.dp)
+                .background(MaterialTheme.colorScheme.onSecondary)
+                .padding(10.dp, 0.dp)
+        ) {
+            Text(text = "Title", modifier = Modifier
+                .wrapContentSize()
+                .align(Alignment.Center))
+            Button(
+                modifier = Modifier
+                    .size(35.dp)
+                    .align(Alignment.CenterEnd),
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
+                contentPadding = PaddingValues(0.dp),
+            ) {
+                Image(modifier = Modifier.size(25.dp), painter = painterResource(R.drawable.ic_setting), contentDescription = "Setting")
+            }
+        }
+    }
 }
